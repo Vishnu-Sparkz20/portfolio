9041c9b318f20a6a93639071e5d3fe066d7d346e
Vishnu-Sparkz20 <vishnujayakrishnaraj05@gmail.com>
2025-05-13 05:12:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var Typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web Developer", "Java Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 500,
+    loop: true
+
+})
