1b82f7d71a6493402a0db0b38b88f3c0e4156b50
Vishnu-Sparkz20 <vishnujayakrishnaraj05@gmail.com>
2026-04-16 20:34:12 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var Typed = new Typed(".text", {
+    strings: ["Software Testing", "Automation", "Manual Testing"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 500,
+    loop: true
+
+})
