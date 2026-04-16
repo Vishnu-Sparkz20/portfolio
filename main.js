a85c610d0595e7a6e0c0aca640aee3b3e8c6c138
Vishnu-Sparkz20 <vishnujayakrishnaraj05@gmail.com>
2026-04-16 23:19:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var Typed = new Typed(".text", {
     strings: ["Software Testing", "Automation", "Manual Testing"],
     typeSpeed: 50,
@@ -7,13 +6,3 @@ var Typed = new Typed(".text", {
     loop: true
 
 })
-=======
-var Typed = new Typed(".text", {
-    strings: ["Software Testing", "Frontend Development", "Software Development"],
-    typeSpeed: 50,
-    backSpeed: 50,
-    backDelay: 500,
-    loop: true
-
-})
->>>>>>> 045ea25f3029ec03db409dc5337c176c9462a5fc
